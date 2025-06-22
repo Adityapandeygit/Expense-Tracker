0516ae878c674a981a5c7e06f43b2a15677c0739
Aditya Pandey <adityapandeyad@outlook.com>
2025-06-22 19:35:18 +0530

    Backend Complte

--- backened/middleware/authMiddleware.js
+++ backened/middleware/authMiddleware.js
@@ -1,25 +1,18 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-const protect = async (req, res, next) => {
-    let token;
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith("Bearer")
-    ) {
-        try {
-            token = req.headers.authorization.split(" ")[1];
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.id).select("-password");
-            next();
-        } catch (error) {
-            res.status(401).json({ message: "Not authorized, token failed" });
-        }
-    }
+exports.protect = async (req, res, next) => {
+  let { token } = req.headers;
 
-    if (!token) {
-        res.status(401).json({ message: "Not authorized, no token" });
-    }
+  if (!token) {
+    return res.status(401).json({ message: "Not authorised, no token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Not authorized, token failed" });
+  }
 };
-
-module.exports = protect;
--- backened/routes/authRoutes.js
+++ backened/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
-
-
+const { protect } = require("../middleware/authMiddleware");
+const upload = require("../middleware/uploadMiddleware");
 const {
     registerUser,
     loginUser,
@@ -8,12 +8,19 @@ const {
 } = require("../controllers/authController");
 
 const router = express.Router();
-const protect = require("../middleware/authMiddleware");
 
-router.post("/register",registerUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/getUser", protect, getUserInfo);  
 
-router.post("/login",loginUser);
-
- router.get("/getUser",protect,getUserInfo);
+router.post("/upload-image", upload.single("image"), (req,res)=>{
+    if(!req.file){
+        return res.status(400).json({message: "No file uploaded"});
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+        req.file.filename
+    }`;
+    res.status(200).json({imageUrl});
+})
 
 module.exports = router;
